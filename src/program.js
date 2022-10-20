5d057c243b1e87b0b411a20a23393cbeda0ea8a8
Korev Dmitriy <korev.dmitriy1@yandex.ru>
2022-10-20 22:49:56 +0300

    configure commander package

--- src/program.js
+++ src/program.js
@@ -1,3 +1,17 @@
+import { program } from 'commander'
+import packageJson from '../package.json' assert { type: 'json' }
+
+const { bin, description, version } = packageJson
+
+program
+  .name(Object.keys(bin)[0])
+  .description(description)
+  .version(version)
+  .argument('<path>', 'path to package.json or folder where this file is located file is located')
+
 export const startProgram = () => {
-  console.log('program has been started')
+  program.parse()
+  const path = program.args[0]
+
+  console.log('program has been started', path)
 }
